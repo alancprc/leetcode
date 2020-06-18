1b908d7bd7f672fa1badaacc152578eb8c536b42
alan liang @vapps <alan.liang@xcerra.com>
2020-06-18 17:32:41 +0800

    add solution 1028, not finish yet.

---
+++ algorithm-1028-recover-a-tree-from-preorder-traversal/solution.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <condition_variable>
+#include <cstdio>
+#include <queue>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <regex>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+const int null = 0;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+class Solution
+{
+ public:
+  TreeNode *recoverFromPreorder(string S) { return new TreeNode(0); }
+};
+
+vector<int> print(TreeNode *tree)
+{
+  // middle, left, right
+  queue<TreeNode *> nodes;
+  vector<int> result;
+
+  nodes.push(tree);
+
+  while (!nodes.empty()) {
+    auto ptr = nodes.front();
+    nodes.pop();
+    if (ptr != nullptr) {
+      result.push_back(ptr->val);
+      nodes.push(ptr->left);
+      nodes.push(ptr->right);
+    } else {
+      result.push_back(0);
+    }
+  }
+  return result;
+}
---
+++ algorithm-1028-recover-a-tree-from-preorder-traversal/test.cpp
@@ -0,0 +1,57 @@
+#include <gmock/gmock.h>
+#include <chrono>
+#include <future>
+#include <iostream>
+#include "solution.h"
+
+using namespace std;
+using namespace ::testing;
+
+typedef Solution Foo;
+
+class LeetCode : public ::testing::Test
+{
+ public:
+  void SetUp() {}
+
+ protected:
+  Foo foo;
+  const int loopNum = 1e6;
+};
+
+TEST_F(LeetCode, printTest)
+{
+    TreeNode root(1);
+    TreeNode n401(401);
+    TreeNode n349(349);
+    TreeNode n88(88);
+    TreeNode n90(90);
+    root.left = &n401;
+    n401.left = &n349;
+    n401.right = &n88;
+    n349.left = &n90;
+
+    EXPECT_THAT(print(&root), ElementsAre(1, 401, 0, 349, 88, 90));
+}
+
+TEST_F(LeetCode, test0)
+{
+  string input = "1-2--3--4-5--6--7";
+  auto tree = foo.recoverFromPreorder(input);
+  EXPECT_THAT(print(tree), ElementsAre(1, 2, 5, 3, 4, 6, 7));
+}
+
+TEST_F(LeetCode, test1)
+{
+  string input = "1-2--3---4-5--6---7";
+  auto tree = foo.recoverFromPreorder(input);
+  EXPECT_THAT(print(tree), ElementsAre(1,2,5,3,0,6,0,4,0,7));
+}
+
+TEST_F(LeetCode, test2)
+{
+  string input = "1-401--349---90--88";
+  auto tree = foo.recoverFromPreorder(input);
+  EXPECT_THAT(print(tree), ElementsAre(1,401,0,349,88,90));
+}
+
