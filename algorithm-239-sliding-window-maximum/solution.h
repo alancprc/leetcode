29eaaaed2524f716337b52284b81fd488ee89204
Rongshun Liang <rongshun.liang.sx@renesas.com>
2023-06-07 15:01:23 +0800

    239-sliding-window-maximum with map

--- algorithm-239-sliding-window-maximum/solution.h
+++ algorithm-239-sliding-window-maximum/solution.h
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <set>
 #include <numeric>
 #include <queue>
 #include <regex>
@@ -25,6 +26,32 @@ using namespace std;
 class Solution
 {
  public:
+  // save window with map
+  vector<int> maxSlidingWindow(vector<int>& nums, int k)
+  {
+    vector<int> result;
+    result.reserve(nums.size() - k + 1);
+
+    typedef vector<int>::iterator ptr;
+    ptr itL = nums.begin();
+    ptr itR = itL + k;
+
+    map<int, int> window;
+    for (auto it = itL; it != itR; ++it) ++window[*it];
+
+    result.push_back((window.rbegin())->first);
+    for (; itR < nums.end(); ++itL, ++itR) {
+      int newVal = *itR;
+      int oldVal = *itL;
+
+      ++window[newVal];
+      --window[oldVal];
+      if (window[oldVal] == 0) window.erase(oldVal);
+
+      result.push_back((window.rbegin())->first);
+    }
+    return result;
+  }
   vector<int> maxSlidingWindowMaxElement(vector<int>& nums, int k)
   {
     vector<int> result;
@@ -45,14 +72,6 @@ class Solution
       }
       result.push_back(*itM);
     }
-    return result;
-  }
-  // map/set
-  vector<int> maxSlidingWindow(vector<int>& nums, int k)
-  {
-    vector<int> result;
-    result.reserve(nums.size() - k + 1);
-
     return result;
   }
 };
